ee6ea3a847e1f8169bcaaa3357574be4ef9701c0
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-07-28 10:35:43 +0300

    Setup environment

--- moneymanager/asgi.py
+++ moneymanager/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moneymanager.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moneymanager.settings")
 
 application = get_asgi_application()
