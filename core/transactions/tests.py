f7d8ea3a766cc09b848e0f7738442e43d84a075d
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-08-01 12:18:09 +0300

    Test authorization for transaction categories view

--- core/transactions/tests.py
+++ core/transactions/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+
+class TransactionCategoryViewTests(TestCase):
+    def test_unauthorized(self):
+        response = self.client.get(reverse("transaction-category-list"))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
