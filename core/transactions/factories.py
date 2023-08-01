fad02f6e29db32bff6c14aaee649b47a49dcc67b
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-08-01 17:08:24 +0300

    Working TransactionCategoryFactory, additional tests

--- core/transactions/factories.py
+++ core/transactions/factories.py
@@ -1,6 +1,7 @@
 import factory
 
 from ..constants import TransactionType
+from .models import TransactionCategory
 
 DEFAULT_ACCOUNT_PASSWORD = "default_password"
 
@@ -14,10 +15,14 @@ class AccountFactory(factory.django.DjangoModelFactory):
 
 
 class TransactionCategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = TransactionCategory
+
+    account = factory.SubFactory(AccountFactory)
     transaction_type = TransactionType.OUTCOME[0]
     parent_category = factory.SubFactory(
-        "moneymanager.core.transactions.factories.TransactionCategoryFactory",
-        transaction_type=factory.SelfAttribute("transaction_type"),
+        "core.transactions.factories.TransactionCategoryFactory",
+        transaction_type=factory.SelfAttribute("..transaction_type"),
     )
     name = factory.sequence(lambda n: "Category %d" % n)
     color = factory.Faker("color")
--- core/transactions/tests.py
+++ core/transactions/tests.py
@@ -3,7 +3,12 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from .factories import AccountFactory, TransactionCategoryFactory, DEFAULT_ACCOUNT_PASSWORD
+from .factories import (
+    DEFAULT_ACCOUNT_PASSWORD,
+    AccountFactory,
+    TransactionCategoryFactory,
+)
+from ..constants import TransactionType
 
 
 class TransactionCategoryViewTests(TestCase):
@@ -15,10 +20,45 @@ class TransactionCategoryViewTests(TestCase):
         )
 
     def test_unauthorized(self):
+        """Try to get categories list without providing authorization credentials."""
         self.client.logout()
         response = self.client.get(reverse("transaction-category-list"))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_empty_list(self):
+    def test_no_categories(self):
+        """Response should be an empty list if there are on categories."""
         response = self.client.get(reverse("transaction-category-list"))
         self.assertListEqual(response.json(), [])
+
+    def test_categories_list_amount(self):
+        """Response list should contain correct amount of items."""
+        categories_amount = 5
+        TransactionCategoryFactory.create_batch(
+            categories_amount,
+            account=self.account,
+            parent_category=None,
+        )
+        response = self.client.get(reverse("transaction-category-list"))
+        response_list = response.json()
+        self.assertIsInstance(response_list, list)
+        self.assertEqual(len(response_list), categories_amount)
+
+    def test_add_subcategory(self):
+        """Subcategory should be created and have the same transaction_type."""
+        parent_category = TransactionCategoryFactory(
+            account=self.account,
+            parent_category=None,
+            transaction_type=TransactionType.INCOME[0],
+        )
+        response = self.client.post(
+            reverse("transaction-category-subcategories", args=(parent_category.id,)),
+            {"name": "Subcategory"},
+        )
+        self.assertDictContainsSubset(
+            {
+                "parent_category": parent_category.id,
+                "transaction_type": parent_category.transaction_type,
+                "name": "Subcategory",
+            },
+            response.json(),
+        )
