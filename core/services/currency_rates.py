cba27bd51f7d0dff4c31a47b9e6fded97f3c41d4
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-08-07 17:04:28 +0300

    AlfaBankNationalRates implementation

--- core/services/currency_rates.py
+++ core/services/currency_rates.py
@@ -1,8 +1,10 @@
 from abc import ABCMeta, abstractmethod
-from datetime import datetime, timedelta, time
+from datetime import datetime, time, timedelta
 from decimal import Decimal
-from typing import TypeVar
+from typing import Generic, TypeVar
 
+import requests
+from django.conf import settings
 from django.core.cache import cache
 
 from ..constants import CurrencyCode
@@ -10,7 +12,7 @@ from ..constants import CurrencyCode
 T = TypeVar("T")
 
 
-class BaseRates(metaclass=ABCMeta):
+class BaseRates(Generic[T], metaclass=ABCMeta):
     def _seconds_to_midnight(self) -> int:
         now = datetime.utcnow()
         midnight = datetime.combine(now + timedelta(days=1), time())
@@ -30,3 +32,26 @@ class BaseRates(metaclass=ABCMeta):
     @abstractmethod
     def get_rate(self, cur_from: CurrencyCode, cur_to: CurrencyCode) -> Decimal:
         pass
+
+
+class AlfaBankNationalRates(BaseRates[dict[str, Decimal]]):
+    def fetch_data(self) -> dict[str, Decimal]:
+        res = requests.get(settings.ALFA_BANK_NATIONAL_RATES_URL)
+        return {
+            currency["iso"]: Decimal(currency["rate"]) / Decimal(currency["quantity"])
+            for currency in filter(
+                lambda currency: currency["iso"] in CurrencyCode.values,
+                res.json()["rates"],
+            )
+        }
+
+    def get_rate(self, cur_from: CurrencyCode, cur_to: CurrencyCode) -> Decimal:
+        if cur_from == cur_to:
+            return Decimal(1)
+        rates = self.get_data()
+        if cur_to is CurrencyCode.BYN:
+            return rates[cur_from.value]
+        if cur_from is CurrencyCode.BYN:
+            return Decimal(1) / rates[cur_to.value]
+        rate_to_byn = rates[cur_from.value]
+        return rate_to_byn / rates[cur_to.value]
--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -148,3 +148,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
 }
+
+ALFA_BANK_NATIONAL_RATES_URL = env(
+    "ALFA_BANK_NATIONAL_RATES_URL",
+    default="https://developerhub.alfabank.by:8273/partner/1.0.1/public/nationalRates",
+)
