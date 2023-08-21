4391e3feb90b735b4a85a851e40d7c2ccb46103e
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-08-21 15:53:39 +0300

    Json export

--- core/export/serializers.py
+++ core/export/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..transactions.models import Transaction
+from ..transactions.models import Transaction, TransactionCategory
 
 
 class TransacationCsvSerializer(serializers.ModelSerializer):
@@ -19,4 +19,36 @@ class TransacationCsvSerializer(serializers.ModelSerializer):
             "amount",
             "transaction_time",
             "comment",
+        )
+
+
+class TransactionJsonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = (
+            "currency",
+            "amount",
+            "transaction_time",
+            "comment",
+        )
+
+
+class _RecursiveField(serializers.Serializer):
+    def to_representation(self, instance):
+        serializer = self.parent.parent.__class__(instance, context=self.context)
+        return serializer.data
+
+
+class CategoryJsonSerializer(serializers.ModelSerializer):
+    subcategories = _RecursiveField(many=True)
+    transactions = TransactionJsonSerializer(many=True)
+
+    class Meta:
+        model = TransactionCategory
+        fields = (
+            "transaction_type",
+            "name",
+            "display_order",
+            "icon",
+            "color",
         )
--- core/export/services.py
+++ core/export/services.py
@@ -1,12 +1,14 @@
 import csv
 import json
-from collections.abc import Iterable, Mapping
+from collections.abc import Iterable
 from typing import TypeVar
 
 from django.http import StreamingHttpResponse
+from json_stream import streamable_list
+from rest_framework.response import Response
 
-from ..transactions.models import Transaction
-from .serializers import TransacationCsvSerializer
+from ..transactions.models import Transaction, TransactionCategory
+from .serializers import CategoryJsonSerializer, TransacationCsvSerializer
 
 T = TypeVar("T")
 
@@ -36,5 +38,22 @@ def csv_response(transactions: Iterable[Transaction]):
     return StreamingHttpResponse(
         csv_generator(transactions),
         content_type="text/csv",
-        headers={"Content-Disposition": 'attachment; filename="transactions.csv"'},
+        headers={"Content-Disposition": 'attachment; filename="Transactions.csv"'},
+    )
+
+
+@streamable_list
+def json_generator():
+    primary_categories = TransactionCategory.objects.filter(
+        parent_category__isnull=True
+    )
+    for category in primary_categories.iterator():
+        yield CategoryJsonSerializer(category).data
+
+
+def json_response():
+    return Response(
+        json.dumps(json_generator()),
+        content_type="application/json",
+        headers={"Content-Disposition": 'attachment; filename="Moneyger.json"'},
     )
