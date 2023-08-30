63095d3de59b785a332abc6675dfa850955cbede
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-08-30 15:05:55 +0300

    Remove jwt urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("browsable-api-auth/", include("rest_framework.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
