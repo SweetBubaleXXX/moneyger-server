0c6e6daa4ef42a505063b91868eda79d8f0f757e
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-08-30 15:00:21 +0300

    Email field factory

--- core/transactions/tests/factories.py
+++ core/transactions/tests/factories.py
@@ -12,6 +12,7 @@ class AccountFactory(factory.django.DjangoModelFactory):
         model = "accounts.Account"
 
     username = factory.Sequence(lambda n: f"User {n}")
+    email = factory.Sequence(lambda n: f"{n}@example.org")
     password = factory.django.Password(DEFAULT_ACCOUNT_PASSWORD)
 
 
