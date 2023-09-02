3caed22820914881155fc5ff7f040afdf0b529cc
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-09-02 16:28:37 +0300

    Add webauthn

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("browsable-api-auth/", include("rest_framework.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
+    path("webauthn/", include("djoser.webauthn.urls")),
 ]
--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "rest_framework",
     "djoser",
+    "djoser.webauthn",
     "drf_spectacular",
     "drf_spectacular_sidecar",
     "django_filters",
