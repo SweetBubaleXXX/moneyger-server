dfd7873dfacdcd8ad2be85110299f7632174438e
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-09-03 15:53:33 +0300

    CORS headers middleware

--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "corsheaders",
     "rest_framework",
     "rest_framework.authtoken",
     "djoser",
@@ -60,6 +61,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -152,6 +154,10 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = DEBUG
+
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
 EMAIL_CONFIG = env.email(default="consolemail://")
 
