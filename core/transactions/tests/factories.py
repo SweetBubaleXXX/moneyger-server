5d1d14b690d48b19b3d9dfa253c067124486ef07
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-10-09 20:44:18 +0300

    Remove redefinition of AccountFactory

--- core/transactions/tests/factories.py
+++ core/transactions/tests/factories.py
@@ -1,19 +1,10 @@
 import factory
 from django.utils import timezone
 
-from accounts.tests.factories import DEFAULT_ACCOUNT_PASSWORD, AccountFactory
+from accounts.tests.factories import AccountFactory
 
 from ...constants import CurrencyCode, TransactionType
 from ..models import Transaction, TransactionCategory
-
-
-class AccountFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = "accounts.Account"
-
-    username = factory.Sequence(lambda n: f"User {n}")
-    email = factory.Sequence(lambda n: f"{n}@example.org")
-    password = factory.django.Password(DEFAULT_ACCOUNT_PASSWORD)
 
 
 class TransactionCategoryFactory(factory.django.DjangoModelFactory):
