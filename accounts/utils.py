b46da844a75d934f68ed149af6732aaaf3f4886a
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-10-21 09:27:06 +0300

    Secure refresh token cookie

--- accounts/utils.py
+++ accounts/utils.py
@@ -10,4 +10,5 @@ def set_refresh_token_cookie(response: Response):
         expires=jwt_settings.REFRESH_TOKEN_LIFETIME,
         samesite=settings.AUTH_COOKIE_SAMESITE,
         httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+        secure=settings.AUTH_COOKIE_SECURE,
     )
