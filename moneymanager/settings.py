d973b683ae4b41e8b0e3623d01d6a04a8d1f7f50
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-10-23 15:16:59 +0300

    Fix refresh token lifetime

--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -222,7 +222,7 @@ DJOSER = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "AUTH_HEADER_TYPES": (
         "Bearer",
