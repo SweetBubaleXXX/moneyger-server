7c8cae9bd724b153d4f0753de401ebad2b6da0df
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-12-19 14:26:50 +0300

    Fix message format in MessagesProducer

--- core/services/notifications/messages.py
+++ core/services/notifications/messages.py
@@ -8,10 +8,17 @@ from .publishers import PublisherMessage
 
 class MessagesProducer(Producer):
     def add_new_message(self, message: SerializedMessage) -> Self:
+        message_body = {
+            "id": message["message_id"],
+            "sender": message["user"],
+            "from_admin": message["is_admin"],
+            "text": message["message_text"],
+            "timestamp": message["timestamp"],
+        }
         self.publisher.add_message(
             PublisherMessage(
                 routing_key="message.event.sent",
-                body=json.dumps(message),
+                body=json.dumps(message_body),
             )
         )
         return self
